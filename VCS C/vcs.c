a6a42bc6c84c06da6a2a417754a1d9eb844f6903
Anuj <anujdas2742006@gmail.com>
2026-01-19 00:17:46 +0530

    done until commit logic1

--- VCS C/vcs.c
+++ VCS C/vcs.c
@@ -2,6 +2,17 @@
 #include<string.h>
 #include<direct.h>
 
+int repoexist(){
+    FILE *fp = fopen(".vcs/log.txt", "r");
+
+    if(fp==NULL){
+        return 0;
+    }
+
+    fclose(fp);
+    return 1;
+}
+
 int main(int argc,char* argv[]){
 
     if(argc<2){
@@ -15,24 +26,31 @@ int main(int argc,char* argv[]){
             return 1;
         }
 
-        if(mkdir(".vcs\\commit")==-1){
-            printf("Commit already exists or error occured!!");
-            return 1;
-        }
+        mkdir(".vcs/commit");
 
-        FILE *fp = fopen(".vcs\\log.txt", "w");
+        FILE *fp = fopen(".vcs/log.txt", "w");
         if(fp==NULL){
             printf("Invalid file!!");
             return 1;
         }
 
-        fprintf(fp, "VCS initialized\n");
+        fprintf(fp, "0");
         fclose(fp);
 
-        printf("Initializing empty repository...");
+        printf("Initialized a VCS Repository in .vcs\n");
     }
     else{
-        printf("Unknown Command: %s", argv[1]);
+        if(!repoexist()){
+            printf("Not a ./vcs repository!");
+            return 1;
+        }
+        if(strcmp(argv[1],"commit")==0){
+
+            //Commit logic pending brahh
+
+        }else{
+            printf("Unknown Command: %s", argv[1]);
+        }
     }
 
     return 0;
